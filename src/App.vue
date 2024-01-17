5ee010e127f50021804a28d389d09c653f759733
Drew0004 <drdrewgraphic@gmail.com>
2024-01-17 13:23:41 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+// Importazione 
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+    data() {
+        return { 
+            count: 0 
+        };
+    },
+    components: {
+        // Dichiarazione componente
+        AppHeader
+    }
+};
+</script>
+
+<template>
+    <div>
+        <!-- Utilizzo componente -->
+        <AppHeader/>
+        
+        <h1>Prova</h1>
+        
+        <p>{{count}}</p>
+        
+        <button @click="count++">
+            Incrementa count
+        </button>
+    </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    data() {
+        return { 
+            title: 'Titolo Header' 
+        };
+    },
+};
+</script>
+
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+    </header>
+</template>
+
+<style scoped>
+    header{
+        padding: 20px;
+        background-color: green;
+    }
+    h1{
+        text-align: center;
+    }
+</style>
